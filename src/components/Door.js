92151905ef98717fc2e6820a3c72d635fc533cb1
flaviosenne123 <joaoflausino123@hotmail.com>
2021-03-18 18:38:45 -0300

    door finished

--- src/components/Door.js
+++ src/components/Door.js
@@ -4,24 +4,24 @@ import '../styles/door.css'
 
 export default class Door extends Component {
     number = this.props.number
-    hasGift= this.props.hasGift
-    
-    state ={
+    hasGift = this.props.hasGift
+
+    state = {
         open: false,
         selected: false
     }
 
-    
-    render(){
-        return(
-            <div className = "door-area">
-                <div className={ this.state.selected ? "door-frame selected": "door-frame"}>
-                    {this.hasGift && this.state.open ? <Gift />: ''}
-                    
-                </div>
-                <div className= "door" onClick ={() => this.setState({selected: !this.state.selected})}>
-                    <div className={ this.state.selected ? "number selected": "number"}>{this.number}</div>
-                    <div className={ this.state.selected ? "knob selected": "knob"}></div>
+
+    render() {
+        return (
+            <div className="door-area">
+                <div className={this.state.selected && !this.state.open ? "door-frame selected" : "door-frame"}>
+                    {this.hasGift && this.state.open ? <Gift /> : ''}
+
+                </div> 
+                <div className={this.state.open ? "door open": "door"} onClick={() => this.setState({ selected: !this.state.selected})}>
+                    <div className={this.state.selected ? "number selected" : "number"}>{this.number}</div>
+                    <div onClick = {() => this.setState({open: true})}className={this.state.selected ? "knob selected" : "knob"}></div>
                 </div>
             </div>
         )

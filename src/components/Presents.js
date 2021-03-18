ef4cdad9e82ae83cc1afcacadcc166e7dbb1a521
flaviosenne123 <joaoflausino123@hotmail.com>
2021-03-17 22:06:59 -0300

    create present

---
+++ src/components/Presents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/gift.css'
+
+export default Present => {
+
+    return(
+        <div className="gift">
+            <div className="gift-tie-1"></div>
+            <div className="gift-tie-2"></div>
+            <div className="gift-top"></div>
+            <div className="gift-body"></div>
+        </div>
+    )
+}
